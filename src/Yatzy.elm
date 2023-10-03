71ecd167adbcc8ee959bf2dcbf3d761562dc5846
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-10-03 15:10:14 +0200

    Finish deduplicating code

--- src/Yatzy.elm
+++ src/Yatzy.elm
@@ -10,42 +10,42 @@ chance d1 d2 d3 d4 d5 =
     d1 + d2 + d3 + d4 + d5
 
 
-countScore : Int -> List Int -> Int
-countScore v dice =
+countScore : Int -> Int -> Int -> Int -> Int -> Int -> Int
+countScore v d1 d2 d3 d4 d5 =
     List.Extra.count
         (\die -> die == v)
-        dice
+        [ d1, d2, d3, d4, d5 ]
         * v
 
 
 ones : Int -> Int -> Int -> Int -> Int -> Int
 ones d1 d2 d3 d4 d5 =
-    countScore 1 [ d1, d2, d3, d4, d5 ]
+    countScore 1 d1 d2 d3 d4 d5
 
 
 twos : Int -> Int -> Int -> Int -> Int -> Int
 twos d1 d2 d3 d4 d5 =
-    countScore 2 [ d1, d2, d3, d4, d5 ]
+    countScore 2 d1 d2 d3 d4 d5
 
 
 threes : Int -> Int -> Int -> Int -> Int -> Int
 threes d1 d2 d3 d4 d5 =
-    countScore 3 [ d1, d2, d3, d4, d5 ]
+    countScore 3 d1 d2 d3 d4 d5
 
 
 fours : Int -> Int -> Int -> Int -> Int -> Int
 fours d1 d2 d3 d4 d5 =
-    countScore 4 [ d1, d2, d3, d4, d5 ]
+    countScore 4 d1 d2 d3 d4 d5
 
 
 fives : Int -> Int -> Int -> Int -> Int -> Int
 fives d1 d2 d3 d4 d5 =
-    countScore 5 [ d1, d2, d3, d4, d5 ]
+    countScore 5 d1 d2 d3 d4 d5
 
 
 sixes : Int -> Int -> Int -> Int -> Int -> Int
 sixes d1 d2 d3 d4 d5 =
-    countScore 6 [ d1, d2, d3, d4, d5 ]
+    countScore 6 d1 d2 d3 d4 d5
 
 
 tally : Int -> Int -> Int -> Int -> Int -> Dict Int Int
@@ -54,14 +54,16 @@ tally d1 d2 d3 d4 d5 =
         |> Dict.Extra.frequencies
 
 
+tallyList : Int -> Int -> Int -> Int -> Int -> List ( Int, Int )
+tallyList d1 d2 d3 d4 d5 =
+    [ d1, d2, d3, d4, d5 ]
+        |> Dict.Extra.frequencies
+        |> Dict.toList
+
+
 fourOfAKind : Int -> Int -> Int -> Int -> Int -> Int
 fourOfAKind d1 d2 d3 d4 d5 =
-    let
-        tallies : Dict Int Int
-        tallies =
-            tally d1 d2 d3 d4 d5
-    in
-    tallies
+    tally d1 d2 d3 d4 d5
         |> Dict.Extra.find (\_ count -> count >= 4)
         |> Maybe.map (\( i, _ ) -> i * 4)
         |> Maybe.withDefault 0
@@ -69,12 +71,7 @@ fourOfAKind d1 d2 d3 d4 d5 =
 
 fullHouse : Int -> Int -> Int -> Int -> Int -> Int
 fullHouse d1 d2 d3 d4 d5 =
-    let
-        tallies : Dict Int Int
-        tallies =
-            tally d1 d2 d3 d4 d5
-    in
-    case Dict.toList tallies of
+    case tallyList d1 d2 d3 d4 d5 of
         [ ( pairValue, 2 ), ( tripleValue, 3 ) ] ->
             pairValue * 2 + tripleValue * 3
 
@@ -96,8 +93,7 @@ largeStraight d1 d2 d3 d4 d5 =
 
 pair : Int -> Int -> Int -> Int -> Int -> Int
 pair d1 d2 d3 d4 d5 =
-    tally d1 d2 d3 d4 d5
-        |> Dict.toList
+    tallyList d1 d2 d3 d4 d5
         |> List.reverse
         |> List.Extra.find (\( _, count ) -> count >= 2)
         |> Maybe.map (\( die, _ ) -> die * 2)
@@ -115,12 +111,11 @@ yatzy d1 d2 d3 d4 d5 =
 
 twoPairs : Int -> Int -> Int -> Int -> Int -> Int
 twoPairs d1 d2 d3 d4 d5 =
-    let
-        tallies : Dict Int Int
-        tallies =
-            tally d1 d2 d3 d4 d5
-    in
-    case List.filter (\( _, count ) -> count >= 2) (Dict.toList tallies) of
+    case
+        tallyList d1 d2 d3 d4 d5
+            |> List.filter
+                (\( _, count ) -> count >= 2)
+    of
         [ ( first, _ ), ( second, _ ) ] ->
             (first + second) * 2
 
@@ -132,7 +127,7 @@ threeOfAKind : Int -> Int -> Int -> Int -> Int -> Int
 threeOfAKind d1 d2 d3 d4 d5 =
     tally d1 d2 d3 d4 d5
         |> Dict.Extra.find (\_ count -> count >= 3)
-        |> Maybe.map (\( i, _ ) -> i * 3)
+        |> Maybe.map (\( die, _ ) -> die * 3)
         |> Maybe.withDefault 0
 
 
